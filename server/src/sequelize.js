0a9e3106a593a00b858ec6a86a4ec79c2b5d5a39
Jim Zawisza <zawisza@post.harvard.edu>
2019-08-21 21:40:29 -0400

    Apply security patches recommended by 'npm audit fix'
    This required tweaking the Sequelize setup a bit due to a deprecation
    in that package.

--- server/src/sequelize.js
+++ server/src/sequelize.js
@@ -1,49 +1,12 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 const { Op } = Sequelize;
-const operatorsAliases = {
-  $eq: Op.eq,
-  $ne: Op.ne,
-  $gte: Op.gte,
-  $gt: Op.gt,
-  $lte: Op.lte,
-  $lt: Op.lt,
-  $not: Op.not,
-  $in: Op.in,
-  $notIn: Op.notIn,
-  $is: Op.is,
-  $like: Op.like,
-  $notLike: Op.notLike,
-  $iLike: Op.iLike,
-  $notILike: Op.notILike,
-  $regexp: Op.regexp,
-  $notRegexp: Op.notRegexp,
-  $iRegexp: Op.iRegexp,
-  $notIRegexp: Op.notIRegexp,
-  $between: Op.between,
-  $notBetween: Op.notBetween,
-  $overlap: Op.overlap,
-  $contains: Op.contains,
-  $contained: Op.contained,
-  $adjacent: Op.adjacent,
-  $strictLeft: Op.strictLeft,
-  $strictRight: Op.strictRight,
-  $noExtendRight: Op.noExtendRight,
-  $noExtendLeft: Op.noExtendLeft,
-  $and: Op.and,
-  $or: Op.or,
-  $any: Op.any,
-  $all: Op.all,
-  $values: Op.values,
-  $col: Op.col
-};
 
 module.exports = function (app) {
   const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/RECIPEDB`;
   const sequelize = new Sequelize(connectionString, {
     dialect: 'postgres',
     logging: false,
-    operatorsAliases,
     define: {
       freezeTableName: true
     }
