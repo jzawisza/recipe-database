eef3350daba36a3a828393e2ca3267954d82b4fe
Jim Zawisza <zawisza@post.harvard.edu>
2019-09-27 20:55:47 -0400

    Update preparation step parsing for Skinny Taste importer
    The Skinny Taste web site changed their formatting, which caused the
    old parser to not return anything for the preparation.

--- server/src/services/recipe-import/importers/skinny-taste-importer.js
+++ server/src/services/recipe-import/importers/skinny-taste-importer.js
@@ -1,10 +1,12 @@
 const cheerio = require('cheerio');
+const errors = require('@feathersjs/errors');
 const { getJsonFromHtml, getTitleFromJson, getNumberOfServingsFromJson,
   getIngredientsFromJson, getPreparationFromStepArray, getNotesFromJson, getCaloriesPerServingFromJson } = require('./recipe-parsing');
 
 const SERVES_REGEX = /(\d+)/;
 const CALORIES_REGEX = /(\d+) kcal/;
-const PREPARATION_SELECTOR = 'div[class="instructions"] li'
+const PREPARATION_STEP_KEY = 'recipeInstructions';
+const PREPARATION_STEP_TEXT_KEY = 'text';
 
 class SkinnyTasteImporter{
   import(url, htmlString, shouldImportNotes) {
@@ -15,10 +17,29 @@ class SkinnyTasteImporter{
     let serves = getNumberOfServingsFromJson(recipeJson, SERVES_REGEX);
     let caloriesPerServing = getCaloriesPerServingFromJson(recipeJson, CALORIES_REGEX);
     let ingredients = getIngredientsFromJson(recipeJson);
-    let preparation = getPreparationFromStepArray($(PREPARATION_SELECTOR), $);
+    let preparation = this.getPreparationFromJson(recipeJson);
     let notes = getNotesFromJson(recipeJson, shouldImportNotes);
 
     return { "source" : url, title, ingredients, preparation, serves, caloriesPerServing, notes };
+  }
+
+  // TODO: move this to recipe-parsing.js if other web sites need it
+  getPreparationFromJson(recipeJson) {
+    let prepStepArray = recipeJson[PREPARATION_STEP_KEY];
+    if (!prepStepArray) {
+      throw new errors.GeneralError('Error getting preparation information from JSON');
+    }
+
+    let stepCount = 1;
+
+    return prepStepArray.map(function(prepStep) {
+      let stepText = prepStep[PREPARATION_STEP_TEXT_KEY];
+      if (stepText) {
+        let numberedStepText = `${stepCount}. ${stepText}`;
+        stepCount++;
+        return numberedStepText;
+      }
+    }).join('\n\n');
   }
 }
 
