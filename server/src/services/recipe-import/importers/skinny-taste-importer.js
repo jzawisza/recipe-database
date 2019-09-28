58f2513b751f604a66aee28ddf5c3bbd75e00e45
Jim Zawisza <zawisza@post.harvard.edu>
2019-09-27 22:08:32 -0400

    Support Skinny Taste recipes with preparation step sections
    Those recipes have JSON like the following:
    
    "recipeInstructions": [
        {
          "@type": "HowToSection",
          "name": "Section Name:",
          "itemListElement": [
            {
              "@type": "HowToStep",
              "text": "Prep step 1"
            }
        ]
      }
    ]
    
    Recipes without sections just have the following JSON:
    
    "recipeInstructions": [
      {
        "@type":"HowToStep",
        "text":"Prep step 1"
      }
    ]

--- server/src/services/recipe-import/importers/skinny-taste-importer.js
+++ server/src/services/recipe-import/importers/skinny-taste-importer.js
@@ -1,14 +1,18 @@
 const cheerio = require('cheerio');
 const errors = require('@feathersjs/errors');
 const { getJsonFromHtml, getTitleFromJson, getNumberOfServingsFromJson,
-  getIngredientsFromJson, getPreparationFromStepArray, getNotesFromJson, getCaloriesPerServingFromJson } = require('./recipe-parsing');
+  getIngredientsFromJson, getNotesFromJson, getCaloriesPerServingFromJson } = require('./recipe-parsing');
 
 const SERVES_REGEX = /(\d+)/;
 const CALORIES_REGEX = /(\d+) kcal/;
 const PREPARATION_STEP_KEY = 'recipeInstructions';
 const PREPARATION_STEP_TEXT_KEY = 'text';
+const PREPARATION_STEP_TYPE_KEY = '@type';
+const SECTION_TYPE = 'HowToSection';
+const SECTION_NAME_KEY = 'name';
+const ITEM_LIST_KEY = 'itemListElement';
 
-class SkinnyTasteImporter{
+class SkinnyTasteImporter {
   import(url, htmlString, shouldImportNotes) {
     const $ = cheerio.load(htmlString);
 
@@ -30,17 +34,34 @@ class SkinnyTasteImporter{
       throw new errors.GeneralError('Error getting preparation information from JSON');
     }
 
-    let stepCount = 1;
-
-    return prepStepArray.map(function(prepStep) {
-      let stepText = prepStep[PREPARATION_STEP_TEXT_KEY];
-      if (stepText) {
-        let numberedStepText = `${stepCount}. ${stepText}`;
-        stepCount++;
-        return numberedStepText;
-      }
-    }).join('\n\n');
+    let firstElementType = prepStepArray[0][PREPARATION_STEP_TYPE_KEY];
+    if (firstElementType === SECTION_TYPE) {
+      return prepStepArray.map(function(prepStep) {
+        let sectionText = prepStep[SECTION_NAME_KEY];
+        let sectionStepArray = prepStep[ITEM_LIST_KEY];
+        console.log(sectionStepArray);
+        let sectionPrepSteps = parsePrepStepArray(sectionStepArray);
+        console.log(sectionPrepSteps);
+        return `${sectionText}\n\n${sectionPrepSteps}`;
+      }).join('\n\n');
+    }
+    else {
+      return parsePrepStepArray(prepStepArray);
+    }
   }
+}
+
+function parsePrepStepArray(prepStepArray) {
+  let stepCount = 1;
+
+  return prepStepArray.map(function(prepStep) {
+    let stepText = prepStep[PREPARATION_STEP_TEXT_KEY];
+    if (stepText) {
+      let numberedStepText = `${stepCount}. ${stepText}`;
+      stepCount++;
+      return numberedStepText;
+    }
+  }).join('\n\n');
 }
 
 module.exports = function (options) {
